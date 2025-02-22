575f5392b089e5ea796ec868c15bfaa9acc394be
GabrielDrapor <gabrieldrapor@gmail.com>
2025-02-23 00:15:07 +0800

    add favicon

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,13 @@
 import './globals.css';
 
+export const metadata = {
+  title: 'JCores',
+  description: 'Your personal podcast library',
+  icons: {
+    icon: '/gboy.ico',
+  },
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
